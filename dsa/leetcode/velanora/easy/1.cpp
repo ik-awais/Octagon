320fa18c7e148e9d4dfe256a21741b826315d5cb
coder-Retro <hasnainqadri9c@gmail.com>
2026-06-26 16:12:03 +0500

    Add: LeetCode Easy 1.cpp

---
+++ dsa/leetcode/velanora/easy/1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+using namespace std;
+
+// Go To Leetcode, Go to problems tab, then search "Two Sum"
+
+// Solution Class
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums,int target) {
+        // Your Code Here
+    }
+};
+
+// Main Function
+// (Dont Change Anything in main)
+// (You can change the testCase values and target value for testing)
+int main() {
+    Solution s;
+    vector<int> testCase={2,7,11,15};
+    int target=9;
+    vector<int> answer=s.twoSum(testCase,target);
+    for(int i:answer)
+        cout<<i<<" ";
+    return 0;
+}
