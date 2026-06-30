1a7f241294fcffcf6d3b24bec5ecce56454679d5
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-01 00:05:51 +0500

    Add: Completed Issue 6

--- dsa/pushback_And_resize.cpp
+++ dsa/pushback_And_resize.cpp
@@ -14,13 +14,19 @@ public:
     }
     
     // Replace yourName with your mentioned name in Issue Description:
-    void pushback_yourName(int value) {
-        // Your Code Here
+    void pushback_kaido(int value) {
+        if(len==cap) resize_kaido();
+        ptr[len++]=value;
     }
 
     // Replace yourName with your mentioned name in Issue Description:
-    void resize_yourName() {
-        // Your Code Here
+    void resize_kaido() {
+        if(cap) cap*=2;
+        else    cap++;
+        int* newPtr=new int[cap];
+        for(int i=0;i<len;i++) newPtr[i]=ptr[i];
+        delete[] ptr;
+        ptr=newPtr;
     }
 
     // Dont Touch Anything Else in this Vector Class.
@@ -41,7 +47,7 @@ int main() {
     // Initial Sizes
     cout<<"Initial Sizes (Empty Vectors) :\n";
     // cout<<"Awais    vector size: "<<v1.size()<<'\n';
-    // cout<<"Kaido    vector size: "<<v2.size()<<'\n';
+    cout<<"Kaido    vector size: "<<v2.size()<<'\n';
     // cout<<"Talha    vector size: "<<v3.size()<<'\n';
     // cout<<"Velanora vector size: "<<v4.size()<<'\n';
     // cout<<"Waleeja  vector size: "<<v5.size()<<'\n';
@@ -51,7 +57,7 @@ int main() {
     // Calling pushbacks
     cout<<"\nPushing Values ...\n";
     // v1.pushback_awais    (250011);
-    // v2.pushback_kaido    (253015);
+    v2.pushback_kaido    (253015);
     // v3.pushback_talha    (250027);
     // v4.pushback_velanora (250014);
     // v5.pushback_waleeja  (250005);
@@ -61,7 +67,7 @@ int main() {
     // Resized After push backs
     cout<<"\nSize Increased :\n";
     // cout<<"Awais    vector size: "<<v1.size()<<'\n';
-    // cout<<"Kaido    vector size: "<<v2.size()<<'\n';
+    cout<<"Kaido    vector size: "<<v2.size()<<'\n';
     // cout<<"Talha    vector size: "<<v3.size()<<'\n';
     // cout<<"Velanora vector size: "<<v4.size()<<'\n';
     // cout<<"Waleeja  vector size: "<<v5.size()<<'\n';
@@ -71,7 +77,7 @@ int main() {
     // Printing Values;
     cout<<"\nPrinting Values :\n";
     // cout<<"Awais    : "<<v1[0]<<'\n';
-    // cout<<"Kaido    : "<<v2[0]<<'\n';
+    cout<<"Kaido    : "<<v2[0]<<'\n';
     // cout<<"Talha    : "<<v3[0]<<'\n';
     // cout<<"velanora : "<<v4[0]<<'\n';
     // cout<<"waleeja  : "<<v5[0]<<'\n';
