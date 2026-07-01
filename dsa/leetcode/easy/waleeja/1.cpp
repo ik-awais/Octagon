ecbf76fd341a560b249dc61e329587c272c54c1a
waleeja07-wk <waleeja07@gmail.com>
2026-07-01 02:06:12 -0800

    Update 1.cpp

--- dsa/leetcode/easy/waleeja/1.cpp
+++ dsa/leetcode/easy/waleeja/1.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums,int target) {
         for(int k=0;k<nums.size();k++){
-            for(int j=0;j<nums.size();j++){
+            for(int j=k+1;j<nums.size();j++){
                 if(nums[k]+nums[j]==target){
                     return {k,j};
                 }
