07f8a72b106292bfd57b3381c622218cf2e2217e
Velanora <syd.areej911@gmail.com>
2026-07-01 21:17:34 +0500

    Completed issue #6

--- dsa/pushback_And_resize.cpp
+++ dsa/pushback_And_resize.cpp
@@ -19,6 +19,15 @@ public:
         ptr[len++]=value;
     }
 
+    void pushback_velanora(int value)
+    {
+        if(len == cap)
+        {
+            resize_velanora();
+        }
+        ptr[len++] = value;
+    }
+
     // Replace yourName with your mentioned name in Issue Description:
     void resize_kaido() {
         if(cap) cap*=2;
@@ -27,6 +36,24 @@ public:
         for(int i=0;i<len;i++) newPtr[i]=ptr[i];
         delete[] ptr;
         ptr=newPtr;
+    }
+
+    void resize_velanora()
+    {
+        if(!cap)
+           cap++;
+        else
+           cap *= 2;
+
+        int *newPtr = new int[cap];
+
+        for (int i = 0; i < len; i++)
+        {
+            newPtr[i] = ptr[i];
+        }
+        delete[] ptr;
+        ptr = newPtr;
+        
     }
 
     // Dont Touch Anything Else in this Vector Class.
@@ -49,7 +76,7 @@ int main() {
     // cout<<"Awais    vector size: "<<v1.size()<<'\n';
     cout<<"Kaido    vector size: "<<v2.size()<<'\n';
     // cout<<"Talha    vector size: "<<v3.size()<<'\n';
-    // cout<<"Velanora vector size: "<<v4.size()<<'\n';
+    cout<<"Velanora vector size: "<<v4.size()<<'\n';
     // cout<<"Waleeja  vector size: "<<v5.size()<<'\n';
     // cout<<"Easha    vector size: "<<v6.size()<<'\n';
     // cout<<"Areeba   vector size: "<<v7.size()<<'\n';
@@ -59,7 +86,7 @@ int main() {
     // v1.pushback_awais    (250011);
     v2.pushback_kaido    (253015);
     // v3.pushback_talha    (250027);
-    // v4.pushback_velanora (250014);
+    v4.pushback_velanora (250014);
     // v5.pushback_waleeja  (250005);
     // v6.pushback_easha    (250064);
     // v7.pushback_areeba   (250056);
@@ -69,7 +96,7 @@ int main() {
     // cout<<"Awais    vector size: "<<v1.size()<<'\n';
     cout<<"Kaido    vector size: "<<v2.size()<<'\n';
     // cout<<"Talha    vector size: "<<v3.size()<<'\n';
-    // cout<<"Velanora vector size: "<<v4.size()<<'\n';
+    cout<<"Velanora vector size: "<<v4.size()<<'\n';
     // cout<<"Waleeja  vector size: "<<v5.size()<<'\n';
     // cout<<"Easha    vector size: "<<v6.size()<<'\n';
     // cout<<"Areeba   vector size: "<<v7.size()<<'\n';
@@ -79,7 +106,7 @@ int main() {
     // cout<<"Awais    : "<<v1[0]<<'\n';
     cout<<"Kaido    : "<<v2[0]<<'\n';
     // cout<<"Talha    : "<<v3[0]<<'\n';
-    // cout<<"velanora : "<<v4[0]<<'\n';
+    cout<<"velanora : "<<v4[0]<<'\n';
     // cout<<"waleeja  : "<<v5[0]<<'\n';
     // cout<<"easha    : "<<v6[0]<<'\n';
     // cout<<"areeba   : "<<v7[0]<<'\n';
