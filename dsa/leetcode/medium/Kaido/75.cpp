61e5010d76a4ce626a6149250e122310d213b2cf
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-28 20:37:45 +0500

    feat: add Sort Colors (75) solution for kaido

--- dsa/leetcode/medium/Kaido/75.cpp
+++ dsa/leetcode/medium/Kaido/75.cpp
@@ -6,7 +6,11 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // Your Code Here
+        size_t l=0,m=0,h=nums.size()-1;
+        while(m<=h)
+            if(nums[m]==0)      swap(nums[m++],nums[l++]);
+            else if(nums[m]==1) m++;
+            else                swap(nums[m],nums[h--]);
     }
 };
 
