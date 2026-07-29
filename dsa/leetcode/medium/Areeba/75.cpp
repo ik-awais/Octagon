7f2a206d627e71f11f7cdf34a449bf6e74df2a47
Areeba Sohail <arshkhattak1@gmail.com>
2026-07-29 16:50:58 +0500

    complete the issue 9

--- dsa/leetcode/medium/Areeba/75.cpp
+++ dsa/leetcode/medium/Areeba/75.cpp
@@ -5,8 +5,35 @@ using namespace std;
 // Solution Class
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& nums) 
+    {
         // Your Code Here
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() -1;
+
+        while(mid <= high )
+        {
+            if(nums[mid] == 0)
+            {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else if(nums[mid] == 2)
+            {
+                int swap = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = swap;
+                high--;
+            }
+        }
     }
 };
 
