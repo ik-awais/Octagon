2c910e376625c27bdbb0a1ccbaf0d010c04afa16
Velanora <syd.areej911@gmail.com>
2026-08-01 19:17:19 +0500

    feat : add sort colors (750 solution for velanora

--- dsa/leetcode/medium/Velanora/75.cpp
+++ dsa/leetcode/medium/Velanora/75.cpp
@@ -1,27 +1,45 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 // Solution Class
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        // Your Code Here
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid<=high){
+
+            if(nums[mid] == 0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){ mid++; }
+            else{ // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+
+        }
     }
 };
 
 // Main Function
 // (Dont Change Anything in main)
 // (You can change the nums values for testing)
-int main() {
+int main()
+{
     Solution s;
-    vector<int> nums = {2,0,2,1,1,0};
+    vector<int> nums = {1,1, 1, 0, 1, 1,1};
 
     s.sortColors(nums);
 
-    for(int i=0;i<nums.size();i++){
-        cout<<nums[i]<<" ";
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
     }
-        
+
     return 0;
 }
