616efe7a593dabee4f9cf06d9c50c3602e9e0e23
waleeja07-wk <waleeja07@gmail.com>
2026-07-31 18:00:24 -0800

    add: waleeja - issue9

--- dsa/leetcode/medium/Waleeja/75.cpp
+++ dsa/leetcode/medium/Waleeja/75.cpp
@@ -6,7 +6,14 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // Your Code Here
+       int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j], nums[j+1]);
+                }
+            }
+        }
     }
 };
 
