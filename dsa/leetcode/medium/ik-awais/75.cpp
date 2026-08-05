9e8e772c7ae9a7e2e18245de9b1075a02ba97981
ik-awais <mawaisqq@gmail.com>
2026-08-05 07:20:14 +0500

    chore: Attempted Issue 9

--- dsa/leetcode/medium/ik-awais/75.cpp
+++ dsa/leetcode/medium/ik-awais/75.cpp
@@ -6,7 +6,24 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // Your Code Here
+        int i = 0, j = 0, k = nums.size()-1, temp;
+        while(j<=k)
+        {
+            if(nums[j]==0)
+            {
+                temp = nums[j];
+                nums[j]=nums[i];
+                nums[i++]=temp;
+                j++;
+            }
+            else if(nums[j]==1) {j++;}
+            else if(nums[j]==2)
+            {
+                temp=nums[j];
+                nums[j]=nums[k];
+                nums[k--]=temp;
+            }
+        }
     }
 };
 
