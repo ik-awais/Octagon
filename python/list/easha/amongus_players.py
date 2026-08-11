9bb352502ceb413a584d997d9a93eaba04b5c8af
Velanora <syd.areej911@gmail.com>
2026-08-11 21:47:39 +0500

    Setup issue11

---
+++ python/list/easha/amongus_players.py
@@ -0,0 +1,54 @@
+''' 
+You need to start this game by making a list named as "players" consisting of minimum 5 players initially. 
+Each player is represented as a color e,g blue, green, red, cyan
+'''
+# your code here
+
+def display():
+    '''
+    Use at least 5 list methods/operations 
+    Recommended: # append(), insert(), remove(), pop(), count()
+    '''
+    
+    # 1.append () ---> add a player to end
+    # Example
+    players.append("green")
+    print("\nAfter appending\n",players)
+    
+    # 2. insert(id, val) ---> add a player at a specofic index
+    # your code here
+
+
+
+    # 3 remove(val) ---> remove a specific player by value
+    # your code here
+
+
+
+    # 4 sort() ----> sort the list alphabetically in ascending order
+    # your code here
+
+
+
+    # 5 count(val) ---> counts the presence of a specific value
+    # your code here
+
+
+    # 6 - other methods 
+    # your code here
+
+
+# don't touch the code below
+
+print("=======================================================================")
+print("                       AMONG US PLAYERS MANAGER                        ")
+print("=======================================================================")
+
+print("\nInitial Players:")
+print(players)
+
+display()
+
+print("\n=====================================================================")
+print("                          Program completed!                           ")
+print("=======================================================================")
