a5af9590077e0b2e7b622179664e6de6661f0bd9
ik-awais <mawaisqq@gmail.com>
2026-08-13 14:34:21 +0500

    changed ik-awais to violet-blue

--- python/list/amongus_players.py
+++ python/list/amongus_players.py
@@ -14,20 +14,20 @@ def display():
     # 1.append () ---> add a player to end
     players.append("purple")
     print("\nAfter appending: purple\n",players)
-    players.append("ik-awais")
-    print(f"\nAfter appending ik-awais\n {players}")
+    players.append("violet-blue")
+    print(f"\nAfter appending violet-blue\n {players}")
 
     # 2. insert(id, val) ---> add a player at a specofic index
     players.insert(4,"orange")
     print("\nAfter inserting: orange\n",players)
-    players.insert(0, "ik-awais")
-    print(f"\nAfter inserting ik-awais at index 0\n {players}")
+    players.insert(0, "violet-blue")
+    print(f"\nAfter inserting violet-blue at index 0\n {players}")
 
     # 3 remove(val) ---> remove a specific player by value
     players.remove("cyan")
     print("\nAfter removing: cyan\n",players)
-    players.remove("ik-awais")
-    print(f"\nAfter removing ik-awais\n {players}")
+    players.remove("violet-blue")
+    print(f"\nAfter removing violet-blue\n {players}")
 
     # 4 sort() ----> sort the list alphabetically in ascending order
     players.sort()
@@ -36,8 +36,8 @@ def display():
     # 5 count(val) ---> counts the presence of a specific value
     reds = players.count("red")
     print("\nCount of red:\n",reds)
-    ikawais = players.count("ik-awais")
-    print(f"\nCount of ik-awais: {ikawais}") 
+    violetBlue = players.count("violet-blue")
+    print(f"\nCount of violet-blue: {violetBlue}") 
 
     # 6 - other methods 
     # pop(index) --> Remove an element at a particular index
@@ -53,21 +53,21 @@ def display():
     # Just an experiment for all code-readers to show power of python
     ''' 
     Y'all might have noticed the using remove() method only removed
-    'ik-awais' that happened to be on index 0. But it didn't remove 
+    'violet-blue' that happened to be on index 0. But it didn't remove 
     the element with same value which I had appeneded earlier. 
     So we met a limitation of this method. 
     Now I'll show how we can remove all the elements with same value from
     a list
     '''
-    players.insert(0, "ik-awais")
-    print(f"\nBefore removing all 'ik-awais' from the list:\n {players}")
-    to_remove = "ik-awais"
+    players.insert(0, "violet-blue")
+    print(f"\nBefore removing all 'violet-blue' from the list:\n {players}")
+    to_remove = "violet-blue"
     new_players = [x for x in players if x!= to_remove]
     players = new_players.copy()
     # There's another way to do this job without making another list, but that's little
     # advance and I don't have a deeper understanding of it, so ain't using right now
     # For this experiment to work, I made our list global in line 8. 
-    print(f"\nAfter removing all 'ik-awais' from the list:\n {players}")
+    print(f"\nAfter removing all 'violet-blue' from the list:\n {players}")
 
 # don't touch the code below
 
